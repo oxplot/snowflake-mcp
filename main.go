09916e3638f109ff4f9a60590cf88402b8db6cec
Mansour Behabadi <mansour@oxplot.com>
2025-03-24 21:12:19 +1100

    Revert "Remove junk"
    This reverts commit f559fa939c77f91a0453b75fa3937e97a169b53e.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/jmoiron/sqlx"
 
@@ -37,10 +38,156 @@ func run() error {
 	}
 	connector := gosnowflake.NewConnector(gosnowflake.SnowflakeDriver{}, sfconfig)
 	db := sqlx.NewDb(sql.OpenDB(connector), "snowflake").Unsafe()
+	if err := db.Ping(); err != nil {
+		return fmt.Errorf("Failed to connect to Snowflake: %v", err)
+	}
 
 	// Create MCP server
 
 	mcpServer := server.NewMCPServer("Snowflake", "1.0.0")
+
+	mcpServer.AddResource(mcp.NewResource(
+		"snowflake://",
+		"Database list",
+		mcp.WithResourceDescription("List of databases"),
+		mcp.WithMIMEType("text/plain"),
+	), func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		return getNameList(db, "SHOW TERSE DATABASES", func(name string) mcp.ResourceContents {
+			return mcp.TextResourceContents{
+				URI:      fmt.Sprintf("snowflake://%s", name),
+				MIMEType: "text/plain",
+				Text:     name,
+			}
+		})
+	})
+
+	schemaPat := regexp.MustCompile(`^snowflake://([^/]+)$`)
+	mcpServer.AddResourceTemplate(mcp.NewResourceTemplate(
+		"snowflake://{database-name}",
+		"Schema list in database",
+		mcp.WithTemplateDescription("List of schemas in a database"),
+		mcp.WithTemplateMIMEType("text/plain"),
+	), func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		m := schemaPat.FindStringSubmatch(request.Params.URI)
+		if m == nil {
+			return nil, fmt.Errorf("Invalid URI")
+		}
+		dbName := m[1]
+		return getNameList(db, fmt.Sprintf(`SHOW TERSE SCHEMAS IN DATABASE %s`, dbName), func(name string) mcp.ResourceContents {
+			return mcp.TextResourceContents{
+				URI:      fmt.Sprintf("snowflake://%s/%s", dbName, name),
+				MIMEType: "text/plain",
+				Text:     name,
+			}
+		})
+	})
+
+	tablesPat := regexp.MustCompile(`^snowflake://([^/]+)/([^/]+)/tables$`)
+	mcpServer.AddResourceTemplate(mcp.NewResourceTemplate(
+		"snowflake://{database-name}/{schema-name}/tables",
+		"Table list in schema",
+		mcp.WithTemplateDescription("List of tables in a schema"),
+		mcp.WithTemplateMIMEType("text/plain"),
+	), func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		m := tablesPat.FindStringSubmatch(request.Params.URI)
+		if m == nil {
+			return nil, fmt.Errorf("Invalid URI")
+		}
+		dbName := m[1]
+		schemaName := m[2]
+
+		return getNameList(db, fmt.Sprintf(`SHOW TERSE TABLES IN SCHEMA %s.%s`, dbName, schemaName), func(name string) mcp.ResourceContents {
+			return mcp.TextResourceContents{
+				URI:      fmt.Sprintf("snowflake://%s/%s/table/%s", dbName, schemaName, name),
+				MIMEType: "text/plain",
+				Text:     name,
+			}
+		})
+	})
+
+	viewsPat := regexp.MustCompile(`^snowflake://([^/]+)/([^/]+)/views$`)
+	mcpServer.AddResourceTemplate(mcp.NewResourceTemplate(
+		"snowflake://{database-name}/{schema-name}/views",
+		"View list in schema",
+		mcp.WithTemplateDescription("List of views in a schema"),
+		mcp.WithTemplateMIMEType("text/plain"),
+	), func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		m := viewsPat.FindStringSubmatch(request.Params.URI)
+		if m == nil {
+			return nil, fmt.Errorf("Invalid URI")
+		}
+		dbName, schemaName := m[1], m[2]
+		return getNameList(db, fmt.Sprintf(`SHOW TERSE TABLES IN SCHEMA %s.%s`, dbName, schemaName), func(name string) mcp.ResourceContents {
+			return mcp.TextResourceContents{
+				URI:      fmt.Sprintf("snowflake://%s/%s/view/%s", dbName, schemaName, name),
+				MIMEType: "text/plain",
+				Text:     name,
+			}
+		})
+	})
+
+	defPat := regexp.MustCompile(`^snowflake://([^/]+)/([^/]+)/(?:view|table)/([^/]+)$`)
+	vtDefHandler := func(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+		m := defPat.FindStringSubmatch(request.Params.URI)
+		if m == nil {
+			return nil, fmt.Errorf("Invalid URI")
+		}
+		dbName, schemaName, tableName := m[1], m[2], m[3]
+		rows, err := db.Queryx(fmt.Sprintf("DESCRIBE TABLE %s.%s.%s", dbName, schemaName, tableName))
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get table def for %s.%s.%s: %v", dbName, schemaName, tableName, err)
+		}
+		defer rows.Close()
+
+		type column struct {
+			Name string `db:"name" json:"name"`
+			Type string `db:"type" json:"type"`
+			Kind string `db:"kind" json:"-"`
+		}
+
+		columns := []column{}
+		for rows.Next() {
+			t := column{}
+			if err = rows.StructScan(&t); err != nil {
+				return nil, fmt.Errorf("Failed to scan rows: %v", err)
+			}
+			if t.Kind != "COLUMN" {
+				continue
+			}
+			columns = append(columns, column(t))
+		}
+
+		b := bytes.NewBuffer(nil)
+		enc := json.NewEncoder(b)
+		enc.SetIndent("", " ")
+		if err := enc.Encode(map[string]any{
+			"columns": columns,
+		}); err != nil {
+			return nil, fmt.Errorf("Failed to marshal result: %v", err)
+		}
+
+		return []mcp.ResourceContents{
+			mcp.TextResourceContents{
+				URI:      request.Params.URI,
+				MIMEType: "application/json",
+				Text:     b.String(),
+			},
+		}, nil
+	}
+
+	mcpServer.AddResourceTemplate(mcp.NewResourceTemplate(
+		"snowflake://{database-name}/{schema-name}/table/{table-name}",
+		"Table definition",
+		mcp.WithTemplateDescription("Definition of a table including columns and column types"),
+		mcp.WithTemplateMIMEType("application/json"),
+	), vtDefHandler)
+
+	mcpServer.AddResourceTemplate(mcp.NewResourceTemplate(
+		"snowflake://{database-name}/{schema-name}/view/{table-name}",
+		"View definition",
+		mcp.WithTemplateDescription("Definition of a view including columns and column types"),
+		mcp.WithTemplateMIMEType("application/json"),
+	), vtDefHandler)
 
 	// Add a query tool.
 	mcpServer.AddTool(mcp.NewTool(
@@ -111,6 +258,26 @@ func run() error {
 		}, nil
 	})
 	return server.ServeStdio(mcpServer)
+}
+
+func getNameList[T any](db *sqlx.DB, query string, conv func(name string) T) ([]T, error) {
+	rows, err := db.Queryx(query)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to run query '%s': %v", query, err)
+	}
+	defer rows.Close()
+
+	ret := []T{}
+	for rows.Next() {
+		t := struct {
+			Name string `db:"name"`
+		}{}
+		if err = rows.StructScan(&t); err != nil {
+			return nil, fmt.Errorf("Failed to scan rows: %v", err)
+		}
+		ret = append(ret, conv(t.Name))
+	}
+	return ret, nil
 }
 
 func main() {
